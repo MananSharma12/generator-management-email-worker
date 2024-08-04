bd209c268f626652163f4112b5015de7d6dfc303
MananSharma12 <manan.sharma1205@gmail.com>
2024-08-04 22:33:09 +0530

    feat: add sendgrid

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,38 @@
 import { Hono } from 'hono'
+import sgMail from '@sendgrid/mail'
 
-const app = new Hono()
+type Bindings = {
+    SENDGRID_API_KEY: string
+}
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+type HonoEnv = {
+    Bindings: Bindings
+}
+
+const app = new Hono<HonoEnv>()
+
+app.use('*', async (c, next) => {
+    sgMail.setApiKey(c.env.SENDGRID_API_KEY)
+    await next()
+})
+
+app.post('/send', async (c) => {
+    const {userEmail, generatorInfo} = await c.req.json()
+
+    const message = {
+        to: userEmail,
+        from: "Vijay Sharma <noreply@shreehps@gmail.com>",
+        subject: "Generator Service Due",
+        text: `Your generator ${generatorInfo} is due for service. Please schedule a service appointment as soon as possible.`,
+    };
+
+    try {
+        const response = await sgMail.send(message);
+        console.log("Email sent successfully:", response);
+    } catch (error) {
+        console.error("Error sending email:", error);
+        throw new Error("Failed to send email notification");
+    }
 })
 
 export default app
