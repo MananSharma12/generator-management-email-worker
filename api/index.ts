ed7d4db5058c06cf07a7139875bdb1118edce1c3
MananSharma12 <manan.sharma1205@gmail.com>
2024-09-09 22:53:23 +0530

    feat: add dotenv

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,8 @@
 import express, {Request, Response} from "express";
 import sgMail from "@sendgrid/mail";
+import dotenv from "dotenv";
 
+dotenv.config();
 sgMail.setApiKey(process.env.SENDGRID_API_KEY as string);
 
 const app = express();
